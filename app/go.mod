4392df82a6555b8a35c41bbef6cfe51c0cba470c
Vusal <vusa.013@gmail.com>
2021-01-02 17:41:31 +0400

    design modify

---
+++ app/go.mod
@@ -0,0 +1,5 @@
+module chat-app/app
+
+go 1.15
+
+require github.com/gorilla/websocket v1.4.2 // indirect
--- app/main.go
+++ app/main.go
@@ -4,9 +4,14 @@ import (
 	"net/http"
 )
 
+
 func main()  {
 	fs := http.FileServer(http.Dir("../front/dist/"))
 	http.Handle("/", fs)
+
+
+
+	//add ws endpoints
 
 	http.ListenAndServe(":3000", nil)
 }
